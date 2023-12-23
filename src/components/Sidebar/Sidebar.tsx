0537b4b18b0ef3ee2f9a3424d3f4d96f290fe5a0
Piotr Skrzynski <pio.skrzynski@gmail.com>
2023-12-23 11:47:02 +0100

    fix: iccontect styling

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -15,15 +15,9 @@ export const Sidebar = ({ children }: Props) => {
   return (
     <SidebarContext.Provider value={{ isExpanded }}>
       <aside
-<<<<<<< HEAD
-        className={`flex flex-col rounded-tr-lg rounded-br-lg drop-shadow-xl transition-all bg-bgPrimary sm:flex-col sm:top-0 sm:left-0 sm:w-screen m:h-screen m:p-4 ${
-          isExpanded ? "max-w-[240px]" : "max-w-[80px]"
-        } `}
-=======
         className={`hidden items-stretch bg-bgPrimary top-0 left-0 h-screen py-4 rounded-tr-lg rounded-br-lg drop-shadow-xl transition-all ${
           isExpanded ? "max-w-[320px]" : "max-w-[80px]"
         } sm:flex sm:flex-col`}
->>>>>>> main
       >
         <div className={`flex basis-2/12 justify-center`}>
           <Logo />
