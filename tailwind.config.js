ac6152d746666658151b9a7dce3f5fd53e550313
Piotr Skrzynski <pio.skrzynski@gmail.com>
2024-03-29 21:59:31 +0100

    chore: install shadcn components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   important: "#root",
+  darkMode: [""],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./@/**/*.{ts,tsx}"],
   theme: {
-    colors: {
-      primary: "#141414",
-      alternate: "#FBFBFB",
-      bgPrimary: "#FBFBFB",
-      bgSecondary: "#e7e6e6",
-      fontDetail: "#FBFBFB",
-      details: "#B6B6B6",
-      textPrimary: "#4A4A4A",
-      textSecondary: "#B6B6B6",
-      textDetail: "#5A5A5A",
-      textAlternate: "#FBFBFB",
-      redPrimary: "#FF6C6C",
-      redSecondary: "#DD3B3B",
-      bluePrimary: "#6C95FF",
-      blueSecondary: "#3E3BDD",
-      backdropDimmed: "rgba(0,0,0, 0.5)",
-      confirmBasic: "#85FF6D",
-      confirmAlternate: "#22CC00",
-      elementsColor: "#4A4A4A",
-    },
-
     extend: {
       dropShadow: {
         inset: [
@@ -38,7 +18,31 @@ export default {
       transitionProperty: {
         gridTemplateRows: "grid-template-rows",
       },
+
+      colors: {
+        primary: "#141414",
+        alternate: "#FBFBFB",
+        neutral600: "#475569",
+        neutral100: "#F1F5F9",
+        bgPrimary: "#FBFBFB",
+        bgSecondary: "#e7e6e6",
+        fontDetail: "#FBFBFB",
+        details: "#B6B6B6",
+        textPrimary: "#4A4A4A",
+        textSecondary: "#B6B6B6",
+        textDetail: "#5A5A5A",
+        textAlternate: "#FBFBFB",
+        redPrimary: "#FF6C6C",
+        redSecondary: "#DD3B3B",
+        bluePrimary: "#6C95FF",
+        blueSecondary: "#3E3BDD",
+        backdropDimmed: "rgba(0,0,0, 0.5)",
+        confirmBasic: "#85FF6D",
+        confirmAlternate: "#22CC00",
+        elementsColor: "#4A4A4A",
+      },
     },
-    plugins: [],
+
+    plugins: [require("tailwindcss-animate")],
   },
 };
