4716eb4214659fc8976796b0c4fa65ac1082a6fe
Piotr Skrzynski <pio.skrzynski@gmail.com>
2024-08-01 20:43:30 +0200

    removed comments

--- src/setupTest.ts
+++ src/setupTest.ts
@@ -5,26 +5,14 @@ import { handlers } from "./mocks/handlers";
 
 const server = setupServer(...handlers);
 
-// const server = setupServer(...handlers);
-
-console.log("mocking server...");
-
 beforeAll(() => {
-  // Start the interception.
   server.listen();
 });
 
 afterEach(() => {
-  // Remove any handlers you may have added
-  // in individual tests (runtime handlers).
   server.resetHandlers();
 });
 
 afterAll(() => {
-  // Disable request interception and clean up.
   server.close();
-});
-
-server.events.on("request:start", ({ request }) => {
-  console.log("MSW intercepted:", request.method, request.url);
 });
